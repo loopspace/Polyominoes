468e0734cebd45a5625b9a2082ccb8d7e8f0e456
Andrew Stacey <stacey@math.ntnu.no>
2015-05-22 17:41:29 +0100

    Fixed the issue with accuracy; now lists the found shapes in individual canvas elements.  Has a selector to choose how many squares to consider.

--- net.js
+++ net.js
@@ -1,84 +1,90 @@
 var ctx;
+var ndiv;
 var nctx;
-var grid = {
-    x: 13,
-    y: 13,
-}
+var mlvl;
+var grid;
 
 var sqsize = 20;
-var mlvl = 6;
 var halt;
-var ht = 0;
-var wd = 0;
-var rht = 0;
+var ht;
+var wd;
+var rht;
 
-var net = [[(grid.x-1)/2,(grid.y-1)/2],[(grid.x-1)/2 ,(grid.y-1)/2 + 1]];
+var net;
 var dirs = [[0,1],[1,0],[0,-1],[-1,0]];
-var nets = [];
+var nets;
 var adder;
 
+var stime;
+var dtime;
+var ltime;
+var nshapes;
+var nfound;
+
 function init() {
-    var i,fn;
-    var cvs = document.querySelector('#net');
-    cvs.width = grid.x * sqsize;
-    cvs.height = grid.y * sqsize;
+	var cvs;
+	ndiv = document.querySelector('#nets');
+	dtime = document.querySelector('#dtime')
+	ltime = document.querySelector('#ltime')
+	nshapes = document.querySelector('#nshapes');
+    cvs = document.querySelector('#net');
     ctx = cvs.getContext('2d');
-    cvs = document.querySelector('#nets');
-    cvs.width = 2 * grid.x * sqsize;
-    cvs.height = 2 * grid.y * sqsize;
-    nctx = cvs.getContext('2d');
+	var btn = document.querySelector('#start');
+	btn.onclick = start;
+}
+
+function start() {
+    var i,fn,cvs,nmbr;
+	nmbr = document.querySelector('#len');
+	mlvl = nmbr.options[nmbr.selectedIndex].value;
+	grid = {
+		x: 2*mlvl-3,
+		y: 2*mlvl-1,
+	}
+	ht = 0;
+	wd = 0;
+	rht = 0;
+	halt = false;
+	stime = new Date().getTime();
+	ltime.innerHTML = '0';
+	dtime.innerHTML = '0';
+	nfound = 0;
+	net = [[(grid.x-1)/2,(grid.y-1)/2],[(grid.x-1)/2 ,(grid.y-1)/2 + 1]];
+	nets = [];
+    ctx.canvas.width = grid.x * sqsize;
+    ctx.canvas.height = grid.y * sqsize;
+	while (ndiv.firstChild) ndiv.removeChild(ndiv.firstChild);
+
     fn = function () {return ;};
     for (i=mlvl-2; i>0; i--) {
-	fn = makeAdder(i+1,fn);
+		fn = makeAdder(i+1,fn);
     }
     adder = fn;
     draw();
     setInterval(draw,1);
+	return false;
 }
 
 function draw() {
     if (halt) {
-	return;
+		return;
     }
     adder(true);
     while (net.length < mlvl) {
-	adder(true);
+		adder(true);
     }
     var i,j,e,ne,c,d;
     e = true;
-    for (i=0; i < nets.length; i++) {
-	if (checkNet(net,nets[i])) {
+	if (checkNet(net,nets)) {
 	    e = false;
-	    break;
-	};
     }
     if (e) {
-	console.log(net);
-	ne = [];
-	c = [13,13];
-	d = [0,0];
-	for (i=0; i < net.length;i++) {
-	    ne[i] = [net[i][0],net[i][1]];
-	    c[0] = Math.min(net[i][0],c[0]);
-	    c[1] = Math.min(net[i][1],c[1]);
-	    d[0] = Math.max(net[i][0],d[0]);
-	    d[1] = Math.max(net[i][1],d[1]);
+		nets.push(normaliseNet(net));
+		addNet(net);
+		nfound++;
+		ltime.innerHTML = ((new Date().getTime() - stime)/1000).toFixed(3);
+		nshapes.innerHTML = nfound;
 	}
-	nets.push(ne);
-	if ((ht + d[1] - c[1])*sqsize > nctx.canvas.height) {
-	    nctx.canvas.height = (ht + d[1] - c[1] + 2)*sqsize;
-	    ht = 0;
-	    wd = 0;
-	    rht = 0;
-	    nets.forEach(function(nt) {
-		redraw(nt);
-	    });
-	    console.log('redrawing');
-	} else {
-	    redraw(ne);
-	}
-	nctx.restore();
-    }
     clear(ctx);
     ctx.save();
     ctx.fillStyle = '#ccc';
@@ -107,6 +113,7 @@ function draw() {
     }
     ctx.stroke();
     ctx.restore();
+	dtime.innerHTML = ((new Date().getTime() - stime)/1000).toFixed(3);
 }
 
 function makeAdder (lvl,fn) {
@@ -162,56 +169,50 @@ function clear(c) {
     c.restore();
 }
 
-function checkNet(n,m) {
-    var a,b,i,j,k,nn,mm;
-    if (n.length != m.length) {
-	return false;
-    }
-    a = [0,0];
-    for (i=0;i<n.length;i++) {
-	a[0] += n[i][0];
-	a[1] += n[i][1];
-    }
-    a[0] /= n.length;
-    a[1] /= n.length;
-    b = [0,0];
-    for (i=0;i<m.length;i++) {
-	b[0] += m[i][0];
-	b[1] += m[i][1];
-    }
-    b[0] /= m.length;
-    b[1] /= m.length;
-    nn = [];
-    mm = [];
-    for (i=0;i<n.length;i++) {
-	nn[i] = [n[i][0]-a[0],n[i][1]-a[1]]
-	mm[i] = [m[i][0]-b[0],m[i][1]-b[1]]
-    }
-    mm.sort(blockSort);
+function normaliseNet(n) {
+	var nn,i;
+	nn = [];
+	for (i=0;i<n.length;i++) {
+		nn[i] = [n[i][0],n[i][1]];
+	}
+	nn.sort(blockSort);
+	for (i=1;i<nn.length;i++) {
+		nn[i][0] -= nn[0][0];
+		nn[i][1] -= nn[0][1];
+	}
+	nn[0] = [0,0];
+	return nn;
+}
+
+function checkNet(n,ns) {
+    var a,b,i,j,k,nn,nnn,equal;
+	nn = normaliseNet(n);
     for (j=0;j<2;j++) {
-	for (i=0;i<nn.length;i++) {
-	    nn[i] = [nn[i][1],nn[i][0]];
-	}
-	for (k=0;k<4;k++) {
-	    for (i=0;i<nn.length;i++) {
-		nn[i] = [-nn[i][1],nn[i][0]];
-	    }
-	    nn.sort(blockSort);
-	    equal = true;
-	    for (i=0; i< nn.length; i++) {
-		if (nn[i][0] != mm[i][0]) {
-		    equal = false;
-		    break;
+		for (i=0;i<nn.length;i++) {
+			nn[i] = [nn[i][1],nn[i][0]];
 		}
-		if (nn[i][1] != mm[i][1]) {
-		    equal = false;
-		    break;
+		for (k=0;k<4;k++) {
+			for (i=0;i<nn.length;i++) {
+				nn[i] = [-nn[i][1],nn[i][0]];
+			}
+			nnn = normaliseNet(nn);
+			for (l=0;l<ns.length;l++) {			
+				equal = true;
+				for (i=0; i< nnn.length; i++) {
+					if (nnn[i][0] != ns[l][i][0]) {
+						equal = false;
+						break;
+					}
+					if (nnn[i][1] != ns[l][i][1]) {
+						equal = false;
+						break;
+					}
+				}
+				if (equal) {
+					return true;
+				}
+			}
 		}
-	    }
-	    if (equal) {
-		return true;
-	    }
-	}
     }
     return false;
 }
@@ -232,27 +233,35 @@ function blockSort(p,q) {
     return 0;
 }
 
-function redraw(ne) {
-    var c,d;
-    c = [13,13];
+function addNet(ne) {
+    var c,d,n,nctx,ncvs;
+    c = [2*mlvl,2*mlvl];
     d = [0,0];
     for (i=0; i < ne.length;i++) {
-	c[0] = Math.min(ne[i][0],c[0]);
-	c[1] = Math.min(ne[i][1],c[1]);
-	d[0] = Math.max(ne[i][0],d[0]);
-	d[1] = Math.max(ne[i][1],d[1]);
+		c[0] = Math.min(ne[i][0],c[0]);
+		c[1] = Math.min(ne[i][1],c[1]);
+		d[0] = Math.max(ne[i][0],d[0]);
+		d[1] = Math.max(ne[i][1],d[1]);
     }
-    if ((wd + d[0] - c[0] + 2)*sqsize > nctx.canvas.width) {
-	wd = 0;
-	ht += rht+2;
-	rht = 0;
-    }
-    nctx.save();
+	n = [];
+	for (i=0; i< ne.length; i++) {
+		n[i] = [ne[i][0]-c[0],ne[i][1]-c[1]];
+	}
+	d[0] -= c[0];
+	d[1] -= c[1];
+	d[0]++;
+	d[1]++;
+	ncvs = document.createElement('canvas');
+	ncvs.width = d[0]*sqsize;
+	ncvs.height = d[1]*sqsize;
+	ncvs.classList.add('net');
+	nctx = ncvs.getContext('2d');
+	nctx.save();
     nctx.beginPath();
-    ne.forEach(function(a) {
-	nctx.rect((a[0]-c[0] + wd)*sqsize+2,(a[1]-c[1]+ht)*sqsize+2,sqsize-4,sqsize-4);
+    n.forEach(function(a) {
+		nctx.rect(a[0]*sqsize+2,a[1]*sqsize+2,sqsize-4,sqsize-4);
     });
-    wd += d[0] - c[0] + 2;
-    rht = Math.max(rht,d[1] - c[1]);
     nctx.fill();
+	nctx.restore();
+	ndiv.appendChild(ncvs);
 }
